4febd20a6625a31c59d6d82f167ecc3f6dd26f85
par5ul1 <parsuli@zoho.com>
2018-07-27 01:33:45 +0430

    .

--- controllers/calendarController.js
+++ controllers/calendarController.js
@@ -132,7 +132,7 @@ exports.generateCalendar = async function(req, res) {
       summary: calendar.event.name[i]
     })
   }
-  fs.writeFile(`app/public/calendars/${user._id}.ics`, cal.toString(), function(err) {
+  fs.writeFileSync(`/public/uploads/calendars`, cal.toString(), function(err) {
     if (err) throw err
     console.log("Done");
   });
